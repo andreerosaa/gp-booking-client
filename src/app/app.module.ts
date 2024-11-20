baf5d915f0010db340e4c0457d66c0832d542a82
andreerosaa <andrefsantosrosa@gmail.com>
2024-11-20 15:42:16 +0000

    locale pt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,11 +7,16 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { MainComponent } from './components/main/main.component';
 import { HeaderComponent } from './components/header/header.component';
 import { DateTabsComponent } from './components/date-tabs/date-tabs.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import localePt from '@angular/common/locales/pt-PT';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localePt);
 
 @NgModule({
 	declarations: [AppComponent, MainComponent, HeaderComponent, DateTabsComponent],
-	imports: [BrowserModule, AppRoutingModule],
-	providers: [provideAnimationsAsync()],
+	imports: [BrowserModule, AppRoutingModule, MatTabsModule],
+	providers: [{ provide: LOCALE_ID, useValue: 'pt-PT' }, provideAnimationsAsync()],
 	bootstrap: [AppComponent]
 })
 export class AppModule {}
