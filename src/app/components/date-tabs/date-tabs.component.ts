a90aa7ad22bc4568021243297be7ce6c2e44b06e
andreerosaa <andrefsantosrosa@gmail.com>
2025-01-20 10:03:09 +0000

    Selected dates navigation interdependency with tab group and calendar

--- src/app/components/date-tabs/date-tabs.component.ts
+++ src/app/components/date-tabs/date-tabs.component.ts
@@ -1,25 +1,45 @@
-import { Component, inject, model, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectorRef, Component, computed, effect, inject, OnInit, signal, ViewChild } from '@angular/core';
 import { DatesService } from '../../services/dates/dates.service';
 import { MatTabGroup } from '@angular/material/tabs';
 import { AuthService } from '../../services/auth/auth.service';
+import { MatCalendar } from '@angular/material/datepicker';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
 	selector: 'app-date-tabs',
 	standalone: false,
-
 	templateUrl: './date-tabs.component.html',
 	styleUrl: './date-tabs.component.scss'
 })
 export class DateTabsComponent implements OnInit {
 	@ViewChild('tabGroup') tabGroup!: MatTabGroup;
+	@ViewChild(MatCalendar) calendar!: MatCalendar<Date>;
+	@ViewChild('drawer', { static: true }) drawer!: MatDrawer;
 
 	private readonly _datesService = inject(DatesService);
 	private readonly _authService = inject(AuthService);
+	private readonly _changeDetectorRef = inject(ChangeDetectorRef);
 
-	selected = model<Date | null>(null);
+	selectedDate = signal<Date | null>(null);
+	selectedIndex = computed<number>(() =>
+		this.dateRange.findIndex(
+			(date) =>
+				date.getDate() === this.selectedDate()?.getDate() &&
+				date.getMonth() === this.selectedDate()?.getMonth() &&
+				date.getFullYear() === this.selectedDate()?.getFullYear()
+		)
+	);
+	effect = effect(() => {
+		this.selectedIndex();
+		if(!this.drawer.opened) {
+			this.drawer.toggle();
+			this._changeDetectorRef.detectChanges()
+		}
+	})
 	dateRange: Date[] = [];
 	today = new Date();
-	minDate = !this._authService.isLoggedIn() ? this.today : null; //TODO: set min date for logged user to be 1 year behind (env.variable)
+	minDate = this._authService.isLoggedIn() ? new Date(new Date(this.today).setFullYear(this.today.getFullYear() - 1)) : this.today;
+	maxDate = new Date(new Date(this.today).setFullYear(this.today.getFullYear() + 1));
 
 	ngOnInit(): void {
 		this.dateRange = this._datesService.getDateRange();
@@ -27,6 +47,12 @@ export class DateTabsComponent implements OnInit {
 
 	resetSelectedTab() {
 		this.tabGroup.selectedIndex = this.dateRange.findIndex((date) => date.getDate() === this.today.getDate());
-		this.selected.set(this.today);
+		this.selectedDate.set(this.today);
+		this.calendar._goToDateInView(this.today, 'month');
+	}
+
+	setSelectedDate(index: number) {
+		this.selectedDate.set(this.dateRange[index]);
 	}
 }
+	
