202894f963bed6e0bb32a304b670c9cf123d47f4
andreerosaa <andrefsantosrosa@gmail.com>
2025-02-19 01:11:55 +0000

    login component focus

--- src/app/directives/focus/focus.directive.ts
+++ src/app/directives/focus/focus.directive.ts
@@ -1,16 +1,16 @@
 import { Directive, ElementRef, inject, Input } from '@angular/core';
 
 @Directive({
-  selector: '[focus]',
-  standalone: false
+	selector: '[focus]',
+	standalone: false
 })
 export class FocusDirective {
-  private readonly _hostElement = inject(ElementRef)
+	private readonly _hostElement = inject(ElementRef);
 
-  @Input()
-  set focus(focus: boolean) {
-    if (focus) {
-      this._hostElement.nativeElement.focus();
-    }
-  }
+	@Input()
+	set focus(focus: boolean) {
+		if (focus) {
+			setTimeout(() => this._hostElement.nativeElement.focus());
+		}
+	}
 }
